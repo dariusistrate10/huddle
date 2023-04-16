c6f295ce858ce39666375fe20025d972f5ccbbd2
dariusistrate10 <101264872+dariusistrate10@users.noreply.github.com>
2023-04-16 15:40:47 +0300

    Add files via upload

---
+++ Huddle/app.js
@@ -0,0 +1,9 @@
+ScrollReveal({ 
+    reset: false,
+    distance: '60px',
+    duration: 1500,
+    delay: 100
+});
+
+ScrollReveal().reveal('.section-container .card', { delay: 100, origin: 'bottom'});
+ScrollReveal().reveal('.call-to-action', { delay: 5, origin: 'bottom'});
